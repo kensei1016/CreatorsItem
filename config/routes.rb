7a9fc2ab363c2a598373abdf4151e92147ac248d
kensei1016 <kensei1016ab@gmail.com>
2020-03-10 03:27:31 +0900

    routingを一通り生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
-  get 'work_rooms/new'
-  get 'work_rooms/show'
-  get 'work_rooms/edit'
-  get 'work_rooms/update'
-  get 'work_rooms/destroy'
-  get 'work_rooms/index'
-  get 'work_rooms/index_new'
-  get 'work_rooms/index_reccomend'
-  get 'work_rooms/search'
-  get 'creator_items/show'
-  get 'save_items/create'
-  get 'save_items/destroy'
-  get 'relationships/create'
-  get 'relationships/destroy'
-  get 'comments/create'
-  get 'comments/destroy'
-  get 'favorites/create'
-  get 'favorites/destroy'
-  get 'users/show'
-  get 'homes/top'
+  root 'homes#top'
   get 'homes/about'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'mypage', to: 'users#show', as: :mypage
+  resources :users do
+    resource :relationship, only: [:create, :destroy]
+    member do
+      get 'follows'
+      get 'followers'
+    end
+  end
+
+  resources :work_rooms do
+    resource :favorite, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+    collection do
+      get 'new_items',        to: 'work_items#index_new'
+      get 'recommend_items',  to: 'work_items#index_reccomend'
+      get 'search',           to: 'work_items#search'
+    end
+  end
+
+  resources :creator_items, only: :show
 end
