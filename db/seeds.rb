1330a5d2a3d528901be8bb174b3163dcec39213f
kensei1016 <kensei1016ab@gmail.com>
2020-03-10 04:33:20 +0900

    seedの生成（userのみ）

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# -------------------------------------------
+# Fakerを日本語化する
+Faker::Config.locale = :ja
+
+# 指定した文字数の文字列を生成する
+def create_caption(number)
+  Faker::Lorem.characters(number: number)
+end
+# -------------------------------------------
+
+# user
+profiles = [
+  { email: "kensei@example.com",  name: "kensei359",                caption: create_caption(150) },
+  { email: "tonakai@example.com", name: "yukigeshiki_hokkaido_123", caption: create_caption(90) },
+  { email: "tomy@example.com",    name: "tomy_tomy",                caption: create_caption(124) },
+]
+
+profiles.each do |profile|
+  User.find_or_create_by!(email: profile[:email]) do |user|
+    user.email = profile[:email]
+    user.name = profile[:name]
+    user.caption = profile[:caption]
+    user.password = "password"
+  end
+end
+# user/
+
