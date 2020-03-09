588c41466f29f209d9f89967710ddd1eb8fe85f5
kensei1016 <kensei1016ab@gmail.com>
2020-03-09 11:08:49 +0900

    save_itemsの外部参照先修正

--- db/migrate/20200309003220_create_save_items.rb
+++ db/migrate/20200309003220_create_save_items.rb
@@ -2,7 +2,7 @@ class CreateSaveItems < ActiveRecord::Migration[5.2]
   def change
     create_table :save_items do |t|
       t.references :user
-      t.references :creator_item
+      t.references :work_room
 
       t.timestamps
     end
