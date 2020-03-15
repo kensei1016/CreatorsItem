4380af0eb3347220f14c647f6714b606bf0b25bb
kensei1016 <kensei1016ab@gmail.com>
2020-03-15 12:42:14 +0900

    作業部屋投稿機能完了（アイテム投稿、エラーメッセージは後に回す）

--- app/models/work_room.rb
+++ app/models/work_room.rb
@@ -6,4 +6,8 @@ class WorkRoom < ApplicationRecord
   has_many :work_room_images,   dependent: :destroy
 
   accepts_attachments_for :work_room_images, attachment: :image
+
+  acts_as_taggable
+  # acts_as_taggable_on :tags と同じ意味
+
 end
