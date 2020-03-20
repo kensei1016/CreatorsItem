50f0eda4d8cb65b86d44d30d25b70a5e74a1f105
kensei1016 <kensei1016ab@gmail.com>
2020-03-20 17:11:34 +0900

    ユーザ編集機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :creator_genres])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :creator_genre_ids])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:profile_image, :name, :caption, :creator_genre_ids])
   end
 end
