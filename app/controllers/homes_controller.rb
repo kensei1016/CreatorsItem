844f44080ca53f72d6df291897333b1a55406e3d
kensei1016 <kensei1016ab@gmail.com>
2020-03-21 09:30:06 +0900

    トップページ内のアバウトを実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,9 @@ class HomesController < ApplicationController
     # TODO: おすすめ順はいいね、閲覧トラッキング実装後に実装する
     @work_rooms_recomend = WorkRoom.all.includes([:creator_genre]).includes([:user])
     @work_rooms_new = WorkRoom.all.includes([:creator_genre]).includes([:user]).order(created_at: :desc)
+
+    # aboutページ用
+    @creator_genres = CreatorGenre.all
   end
 
   def about
