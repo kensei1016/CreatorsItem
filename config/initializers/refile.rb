c413b1c64a8d1da7c7815104b3995ae69d800373
kensei1016 <kensei1016ab@gmail.com>
2020-03-29 20:51:59 +0900

    Refileの設定を本番環境に制限

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,1 +1,3 @@
-Refile.backends['store'] = Refile::Backend::FileSystem.new('/home/ec2-user/CreatorsItem/shared/uploads')
+if Rails.env.production?
+  Refile.backends['store'] = Refile::Backend::FileSystem.new('/home/ec2-user/CreatorsItem/shared/uploads')
+end
