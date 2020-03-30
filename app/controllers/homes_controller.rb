1b8cdf563c35106f06d8ac23ce2030d04fbb7990
kensei1016 <kensei1016ab@gmail.com>
2020-03-30 19:34:52 +0900

    topページの投稿表示件数変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,10 +3,10 @@ class HomesController < ApplicationController
     # おすすめ＝いいねの件数が多い順
     @work_rooms_recomend = 
       WorkRoom.includes([:creator_genre, :user]).find(
-        Favorite.group(:work_room_id).order('count(work_room_id) desc').limit(8).pluck(:work_room_id)
+        Favorite.group(:work_room_id).order('count(work_room_id) desc').limit(10).pluck(:work_room_id)
       )
     @work_rooms_new = 
-      WorkRoom.all.includes([:creator_genre, :user]).limit(8).order(created_at: :desc)
+      WorkRoom.all.includes([:creator_genre, :user]).limit(10).order(created_at: :desc)
 
     # aboutページ用
     @creator_genres = CreatorGenre.all
